50ac280d67f603e732e909cb09d9206eb4d4f752
Drew Hodson <dehodson@gmail.com>
2019-12-13 20:01:07 -0500

    Add tachyons and factor out Questionnaire type

--- input.hs
+++ input.hs
@@ -2,23 +2,10 @@
 {-# LANGUAGE DeriveAnyClass #-}
 
 import qualified Data.ByteString.Lazy as B
+import Questionnaire
 import GHC.Generics
 import Data.Aeson
 import System.IO
-
-data Section = Section { header :: String
-                       , items :: [String]
-                       } deriving (Show, Generic)
-
-instance ToJSON Section
-instance FromJSON Section
-
-data Questionnaire = Questionnaire { name :: String
-                                   , sections :: [Section]
-                                   } deriving (Show, Generic)
-
-instance ToJSON Questionnaire
-instance FromJSON Questionnaire
 
 sectionNames :: [String]
 sectionNames = [ "Favorite Drinks"
